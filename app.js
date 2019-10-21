8a8c26a92b793f742988ce5c90001e5a0b1534a4
Rohan2599 <201651044@iiitvadodara.ac.in>
2019-10-21 12:15:05 +0530

    added  validation and user signin

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser= require('cookie-parser');
+
+const expressValidator =  require('express-validator');
 require("dotenv").config()
 
 //import
@@ -16,12 +18,12 @@ const app = express()
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
-//routes middleware
 
 app.use('/user',userRoutes);
 
-//db
+
 mongoose.connect(process.env.DATABASE,{
 
 	useNewUrlParser:true,
@@ -34,5 +36,7 @@ const port = process.env.PORT || 8000;
 
 
 app.listen(port,()=>{
-	console.log(`Server is running on port  ${port}`);
+	console.log(`Server is running on port ${port}`);
 })
+
+
--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 
 const User = require('../models/user');
 const {errorHandler} = require('../helpers/dbErrorHandlers');
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req,res)=>{
 
@@ -9,13 +11,42 @@ exports.signup = (req,res)=>{
 	user.save((err,user)=>{
 		if(err){
 			return res.status(400).json({
-				err:errorHandler(err)
+				error :errorHandler(err)
 			})
 		}
+		user.salt = undefined;
+		user.hashed_passsword= undefined;
 
-		res.status(200).json({
-			user
-		})
+		res.json(user)
 	})
+
+}
+
+
+exports.signin = (req,res)=>{
+  
+  const {email,password} = req.body;
+
+  User.findOne({email},(err,user)=>{
+
+  	if(err || !user){
+  		return res.status(400).json({
+  			err:"User with that email does not exists"
+  		})
+  	}
+
+  	if(!user.authenticate(password)){
+  		return res.status(401).json({
+  			error:"Email and password don't match"
+  		})
+  	}
+
+  	const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
+  	res.cookie('t',token, {expire:new Date()+99999});
+  	const {_id,name,email,role} = user;
+
+  	return res.json({token , user:{_id,email,name,role}});
+
+  });
 
 }
